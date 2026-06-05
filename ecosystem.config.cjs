1643be56ea8190f488068eb51f52906d374cd35b
Mirare23 <torawijaya23@gmail.com>
2026-06-05 11:42:34 +0800

    fix: update production port from 3013 to 3014 in ecosystem configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
   apps: [
     {
       name: "richoil@production",
-      port: "3013",
+      port: "3014",
       script: "node_modules/next/dist/bin/next",
       args: "start",
     },
